df9762cc5fd9d816a2b7a511a89190c286626b2b
Olivier TURLIER <o.turlier@gmail.com>
2025-12-01 13:26:58 +0100

    [dyad] add Capacitor for mobile app support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.neonpenguinwag',
+  appName: 'neon-penguin-wag',
+  webDir: 'dist'
+};
+
+export default config;
